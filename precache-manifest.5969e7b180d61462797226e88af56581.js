8435a1839b776d53d7776d09d98a86588a0815c8
DenMinzalirov <den.minzakirov@gmail.com>
2020-06-23 16:16:01 +0300

    start

---
+++ precache-manifest.5969e7b180d61462797226e88af56581.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac361467522ea7f4975c205ca1fc0e6",
+    "url": "/site-asif/index.html"
+  },
+  {
+    "revision": "893b575ef0ad85fdf3d7",
+    "url": "/site-asif/static/css/2.9f2bb942.chunk.css"
+  },
+  {
+    "revision": "b73bff11cb99a591d39c",
+    "url": "/site-asif/static/css/main.43b760fc.chunk.css"
+  },
+  {
+    "revision": "893b575ef0ad85fdf3d7",
+    "url": "/site-asif/static/js/2.0541a865.chunk.js"
+  },
+  {
+    "revision": "ba5c44dd1ae0cf4a36ce821f0b09a39e",
+    "url": "/site-asif/static/js/2.0541a865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73bff11cb99a591d39c",
+    "url": "/site-asif/static/js/main.5be5d234.chunk.js"
+  },
+  {
+    "revision": "f9a1cffee1f434e60ccb",
+    "url": "/site-asif/static/js/runtime-main.a3ab89a2.js"
+  },
+  {
+    "revision": "4070a4de6a3113a2963d7dab08cc76bd",
+    "url": "/site-asif/static/media/19516.4070a4de.otf"
+  }
+]);
